905d28cbd8895c47366e7da22b333ea2ac5b16cc
SergKekuh <serg.kekuh@gmail.com>
2024-03-09 15:17:24 +0200

    Commit first

---
+++ build/CMakeFiles/wxhello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wxhello.dir/wxhello.cpp.o"
+  "CMakeFiles/wxhello.dir/wxhello.cpp.o.d"
+  "wxhello"
+  "wxhello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wxhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
