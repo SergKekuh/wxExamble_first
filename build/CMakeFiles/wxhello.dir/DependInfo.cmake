80e88ebc8d5b86160b31c05b71fb62b7b59f0bb1
SergKekuh <serg.kekuh@gmail.com>
2024-03-15 11:50:31 +0200

    wxWidgets 3.3

--- build/CMakeFiles/wxhello.dir/DependInfo.cmake
+++ build/CMakeFiles/wxhello.dir/DependInfo.cmake
@@ -11,8 +11,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/serg/Documents/wxExample_first/wxhello.cpp" "CMakeFiles/wxhello.dir/wxhello.cpp.o" "gcc" "CMakeFiles/wxhello.dir/wxhello.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
